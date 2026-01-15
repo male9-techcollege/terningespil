57ca43e0c24b4019d7d6a19b787d1c94ee78db5f
Nicolai-Bundgaard-Sorensen <niso21@elev.techcollege.dk>
2026-01-15 13:31:53 +0100

    Merged

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-<<<<<<< HEAD
 import PlayerState from "./components/PlayerState/PlayerState";
-=======
 import DiceRoller from "./components/BlackjackDie/DiceRoller";
->>>>>>> RolingDice-#25
 
 function App() {
   return (
+     <>
     <div>
       <h1>Blackjack med terninger</h1>
       <PlayerState />
     </div>
-  );
-}
-<<<<<<< HEAD
-    <>
-      {/* <ImgComponentByMariePierreLessard src={koala} alt={"koala"} loading={"lazy"} className="test" /> */}
-    </>
-  )
-};
-=======
     <div className="App">
       <h1>Blackjack Dice Game</h1>
       <DiceRoller />
     </div>
+     </>
+    
   );
 }
->>>>>>> RolingDice-#25
 
 export default App;
