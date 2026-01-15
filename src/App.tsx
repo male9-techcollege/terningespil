f5fa214d083839c5d3d1f7b8b078961289787aa4
Marie-Pierre Lessard <male9@elev.techcollege.dk>
2026-01-15 14:35:49 +0100

    Some root styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-/* For testing purposes: 
+/* For testing purposes (image component; test successful)
 import { ImgComponentByMariePierreLessard } from "./components/atoms/img-el";
 import koala from "./_WW236934.jpg";
 import "./App.css";
+This was in the fragment:
+<ImgComponentByMariePierreLessard src={koala} alt={"koala"} loading={"lazy"} className="test" />
+
+For testing purposes (font family, font sizes, ...):
+<h1>h1 h1 h1 h1</h1>
+<h2>h2 h2 h2 h2</h2>
+<h3>h3 h3 h3 h3</h3>
+<p>Text text text</p>
+
 */
+import "./App.scss";
+import "./styles/typography.module.scss";
 
 function App() {
 
   return (
     <>
-      {/* <ImgComponentByMariePierreLessard src={koala} alt={"koala"} loading={"lazy"} className="test" /> */}
     </>
   )
 };
