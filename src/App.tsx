ed6ad86369534631ba6567cbd6d6c1e79f6165c2
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2026-01-15 12:58:53 +0100

    Add DiceRoller and Die components; update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
+<<<<<<< HEAD
 /* For testing purposes: 
 import { ImgComponentByMariePierreLessard } from "./components/atoms/img-el";
 import koala from "./_WW236934.jpg";
 import "./App.css";
 */
+=======
+import DiceRoller from "./components/BlackjackDie/DiceRoller";
+>>>>>>> RolingDice-#25
 
 function App() {
-
   return (
+<<<<<<< HEAD
     <>
       {/* <ImgComponentByMariePierreLessard src={koala} alt={"koala"} loading={"lazy"} className="test" /> */}
     </>
   )
 };
+=======
+    <div className="App">
+      <h1>Blackjack Dice Game</h1>
+      <DiceRoller />
+    </div>
+  );
+}
+>>>>>>> RolingDice-#25
 
 export default App;
